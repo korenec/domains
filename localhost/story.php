13cb713c28142ab0c4111bd5f789e45e755468c1
Vladimir <korenec88@gmail.com>
2022-02-09 22:44:53 +0300

    lesson98

--- localhost/story.php
+++ localhost/story.php
@@ -4,18 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document2</title>
+    <title>Document 2</title>
 </head>
 <body>
 <?php
-$test1 = $_POST["test1"];
-echo <<<HERE
-<p>The spelling mistakes in the text had been highlighted in green.
-The text is finished, but the pictures $test1 will have to be pasted in later.
-You need to demonstrate to the examiners that you have more than a literal understanding of the text.
-The students are reading "Lord of the Flies" as one of their set texts this year.
-At this point in the speech, the minister departed from his prepared text.</p>
-HERE;
+$roll = rand(1,6);
+print "<h1>Выпало число: $roll</h1>";
+
+switch ($roll){
+    case 1:
+        $romValue = "I";
+        break;
+    case 2:
+        $romValue = "II";
+        break;
+    case 3:
+        $romValue = "III";
+        break;
+    case 4:
+        $romValue = "IV";
+        break;
+    case 5:
+        $romValue = "V";
+        break;
+    case 6:
+        $romValue = "VI";
+        break;
+    default:
+    print "Неработает свитч кейс !!";
+}
+
+
+print "<br>";
+print "<img src=die$roll.jpg>";
+print "<br>";
+print "Римское значение: $romValue";
+
 ?>
+<br>
+
 </body>
 </html>
