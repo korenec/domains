ee5405f798856d4d57d4094e2ab286e7e8c80ab0
Vladimir <korenec88@gmail.com>
2022-02-10 21:21:31 +0300

    пройденные примеры теперь закоментированы и остаются в файле index.php

--- localhost/index.php
+++ localhost/index.php
@@ -7,9 +7,57 @@
     <title>Hi user!</title>
 </head>
 <body>
-<h1>This Old Man</h1>
+<h1>This Test page index.php</h1>
 <?php
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* // ------------------------------------------------------------------------------------------
+// Возвращаем значение: функции chorus()  страница 103
+function chorus(){
+$output = <<<HERE
+...with a knick-knack<br>
+paddy-whack<br>
+give a dog a bone<br>
+this old man came rolling home<br><br><br>
+HERE;
+return $output;
+}
+echo chorus();
+*/
+/* //------------------------------------------------------------------------------------------
+// страница 96
+$userName = $_GET['userName']; // пример не работает без этой строки !!!
+if (empty($userName)){
+print <<<HERE
+<form>
+Please enter your name:
+<input type = "text" name = "userName"><br>
+<input type = "submit">
+</form>
+HERE;
+} else {
+print "<h3>Hi there, $userName!</h3>";
+}
+*/
+//--------------------------------------------------------------------------------------------------
+/* // страница 101
 print verse(1);
 print chorus();
 print verse(2);
@@ -52,36 +100,28 @@ this old man came rolling home<br><br><br>
 HERE;
 return $output;
 }
+*/
 
-
-
-
-
-
-
-
-
-
-
-// function verse1(){
-// print <<<HERE
-// old man old man he shet<br>
-// lala old man shet<br><br><br>
-// HERE;
-// }
-// function verse2(){
-// print <<<HERE
-// old man222 old man222 he shet<br>
-// lala old222 man shet<br><br><br>
-// HERE;
-// }
-// function chorus(){
-// print <<<HERE
-// ...with a knick-knack chorus<br><br><br>
-// HERE;
-// }
-
-
+//-------------------------------------------------------------------------------------------------
+/*
+function verse1(){
+print <<<HERE
+old man old man he shet<br>
+lala old man shet<br><br><br>
+HERE;
+}
+function verse2(){
+print <<<HERE
+old man222 old man222 he shet<br>
+lala old222 man shet<br><br><br>
+HERE;
+}
+function chorus(){
+print <<<HERE
+...with a knick-knack chorus<br><br><br>
+HERE;
+}
+*/
 
 ?>
 
--- localhost/story.php
+++ localhost/story.php
@@ -7,41 +7,22 @@
     <title>Document 2</title>
 </head>
 <body>
+
 <?php
-$roll = rand(1,6);
-print "<h1>Выпало число: $roll</h1>";
 
-switch ($roll){
-    case 1:
-        $romValue = "I";
-        break;
-    case 2:
-        $romValue = "II";
-        break;
-    case 3:
-        $romValue = "III";
-        break;
-    case 4:
-        $romValue = "IV";
-        break;
-    case 5:
-        $romValue = "V";
-        break;
-    case 6:
-        $romValue = "VI";
-        break;
-    default:
-    print "Неработает свитч кейс !!";
+if (empty($userName)){
+print <<<HERE
+<form>
+Please enter your name:
+<input type = "text" name = "userName"><br>
+<input type = "submit">
+</form>
+HERE;
+} else {
+print "<h3> Hi there, $userName!</h3>";
 }
-
-
-print "<br>";
-print "<img src=die$roll.jpg>";
-print "<br>";
-print "Римское значение: $romValue";
-
 ?>
-<br>
 
+<br>
 </body>
 </html>
